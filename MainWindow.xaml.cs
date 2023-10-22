1724f6498316d6e240a244ffc30195ae174ead5e
Lenar <leonruscraft@gmail.com>
2023-10-22 15:06:54 +0500

    update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LightAndButton
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+        //Не стал выносить отдельно в класс - ради простоты примера
+        private void Button_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (lamp.Fill == Brushes.Gray)
+            {
+                lamp.Fill = Brushes.Yellow;
+            }
+            else
+            {
+                lamp.Fill = Brushes.Gray;
+            }
         }
     }
 }
